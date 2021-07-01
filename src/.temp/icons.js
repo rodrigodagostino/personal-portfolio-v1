8159cc211965adaf3b18e708c30f05a0fe8446f3
Rodrigo D’Agostino <rodrigo.dagostino@protonmail.com>
2021-07-01 00:26:48 -0300

    Initial commit.

---
+++ src/.temp/icons.js
@@ -0,0 +1,41 @@
+export default {
+	touchiconMimeType: 'image/png',
+	faviconMimeType: 'image/png',
+	precomposed: false,
+	touchicons: [
+		{
+			width: 76,
+			src: '/assets/static/src/favicon.png?width=76&key=800ea1b',
+		},
+		{
+			width: 152,
+			src: '/assets/static/src/favicon.png?width=152&key=800ea1b',
+		},
+		{
+			width: 120,
+			src: '/assets/static/src/favicon.png?width=120&key=800ea1b',
+		},
+		{
+			width: 167,
+			src: '/assets/static/src/favicon.png?width=167&key=800ea1b',
+		},
+		{
+			width: 180,
+			src: '/assets/static/src/favicon.png?width=180&key=800ea1b',
+		},
+	],
+	favicons: [
+		{
+			width: 16,
+			src: '/assets/static/src/favicon.png?width=16&key=4505e86',
+		},
+		{
+			width: 32,
+			src: '/assets/static/src/favicon.png?width=32&key=4505e86',
+		},
+		{
+			width: 96,
+			src: '/assets/static/src/favicon.png?width=96&key=4505e86',
+		},
+	],
+}
