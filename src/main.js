271d5e8904ef00e3b7d1a96de151a9a90efe7418
Rodrigo D’Agostino <rodrigo.dagostino@protonmail.com>
2021-07-10 22:01:34 -0300

    Updated fonts URLs to include only variations being used.

--- src/main.js
+++ src/main.js
@@ -15,15 +15,15 @@ export default function ( Vue, { router, head, isClient } ) {
 	} )
 	head.link.push( {
 		rel: 'stylesheet',
-		href: 'https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap',
+		href: 'https://fonts.googleapis.com/css2?family=Lora:wght@700&display=swap',
 	} )
 	head.link.push( {
 		rel: 'stylesheet',
-		href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;500;600;700&display=swap',
+		href: 'https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@500&display=swap',
 	} )
 	head.link.push( {
 		rel: 'stylesheet',
-		href: 'https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+		href: 'https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;1,400&display=swap',
 	} )
 	// Set default layout as a global component
 	Vue.component( 'Layout', DefaultLayout )
