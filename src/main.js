cceac86083cd9fea2e8a75162273b2a02999f7ba
Rodrigo D’Agostino <rodrigo.dagostino@protonmail.com>
2021-07-14 17:31:54 -0300

    Register new custom directive «reveal-on-scroll»

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+import RevealOnScroll from './directives/reveal-on-scroll'
 
 export default function ( Vue, { router, head, isClient } ) {
 	head.link.push( {
@@ -27,4 +28,7 @@ export default function ( Vue, { router, head, isClient } ) {
 	} )
 	// Set default layout as a global component
 	Vue.component( 'Layout', DefaultLayout )
+
+	// Register custom directives
+	Vue.directive( 'reveal-on-scroll', RevealOnScroll )
 }
