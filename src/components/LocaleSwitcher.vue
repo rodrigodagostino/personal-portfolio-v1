f9176fa048afee15c977c80e048bb002a7e5e32e
Rodrigo D’Agostino <rodrigo.dagostino@gmail.com>
2021-08-06 11:54:09 -0300

    Modify structure to use links instead of a select field

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -1,25 +1,42 @@
 <template>
-	<select v-model="currentLocale" @change="localeChanged">
-		<option v-for="locale in availableLocales" :key="locale" :value="locale">
+	<div class="site-nav__locale-switcher">
+		<g-link
+			v-for="locale in availableLocales"
+			:key="locale"
+			:to="`/${locale}`"
+			class="site-nav__link"
+		>
 			{{ locale }}
-		</option>
-	</select>
+		</g-link>
+		<!-- <select v-model="currentLocale" @change="localeChanged">
+			<option v-for="locale in availableLocales" :key="locale" :value="locale">
+				{{ locale }}
+			</option>
+		</select> -->
+	</div>
 </template>
+
 <script>
 export default {
 	name: 'LocaleSwitcher',
 	data: function () {
 		return {
-			currentLocale: this.$i18n.locale.toString(),
+			// currentLocale: this.$i18n.locale.toString(),
 			availableLocales: this.$i18n.availableLocales,
 		}
 	},
-	methods: {
-		localeChanged() {
-			this.$router.push( {
-				path: this.$tp( this.$route.path, this.currentLocale, true ),
-			} )
-		},
-	},
+	// methods: {
+	// 	localeChanged() {
+	// 		this.$router.push( {
+	// 			path: this.$tp( this.$route.path, this.currentLocale, true ),
+	// 		} )
+	// 	},
+	// },
 }
 </script>
+
+<style scoped>
+.site-nav__locale-switcher {
+	display: flex;
+}
+</style>
